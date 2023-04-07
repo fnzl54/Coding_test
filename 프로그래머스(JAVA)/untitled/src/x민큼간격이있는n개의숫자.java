e1d44c9ee1fff04a122d2389a3ba18548815ab73
fnzl54 <rnjscksdud1@naver.com>
2023-04-07 21:55:56 +0900

    feat : x만큼 간격이 있는 n개의 숫자 level1

---
+++ 프로그래머스(JAVA)/untitled/src/x민큼간격이있는n개의숫자.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class x민큼간격이있는n개의숫자 {
+    public static void main(String[] args) {
+        int x = -4;
+        int n = 2;
+
+        ArrayList<Long> answer = new ArrayList<>();
+
+        long num = x;
+
+        for(int i = 0; i < n; i++){
+            answer.add(num);
+            num += x;
+        }
+
+        System.out.println(answer);
+
+    }
+}
