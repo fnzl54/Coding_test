e4f7a7c3a127162c22d34406291ebb5192773bb9
fnzl54 <rnjscksdud1@naver.com>
2023-04-07 14:07:59 +0900

    feat : 완전 탐색 모의고사 level1

---
+++ 프로그래머스(JAVA)/untitled/src/모의고사.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class 모의고사 {
+    public static void main(String[] args) {
+
+        int[] answers = {1,2,3,4,5};
+
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        int[] one = {1,2,3,4,5};
+        int[] two = {2,1,2,3,2,4,2,5};
+        int[] three = {3,3,1,1,2,2,4,4,5,5};
+        int[] score = {0,0,0};
+
+        for (int i = 0; i < answers.length; i++) {
+            if(answers[i] == one[i%5]) score[0]++;
+            if(answers[i] == two[i%8]) score[1]++;
+            if(answers[i] == three[i%10]) score[2]++;
+        }
+
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
+
+        for (int i  = 0; i < 3; i++) {
+            if (max == score[i]) answer.add(i+1);
+        }
+
+        System.out.println(answer);
+
+    }
+}
