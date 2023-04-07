32b9afa233c2e90dc18f4a517bc8f8478c31713f
fnzl54 <rnjscksdud1@naver.com>
2023-04-07 21:36:51 +0900

    feat : 자릿수더하기 level1

---
+++ 프로그래머스(JAVA)/untitled/src/자릿수더하기.java
@@ -0,0 +1,17 @@
+public class 자릿수더하기 {
+    public static void main(String[] args) {
+        int n = 123;
+
+        int tempAdd = 0;
+
+        String s = Integer.toString(n); //int n을 String으로 변환
+
+        for (int i = 0; i < s.length(); i++){
+            tempAdd +=  Character.getNumericValue(s.charAt(i));
+
+        }
+
+        System.out.println(tempAdd);
+
+    }
+}
