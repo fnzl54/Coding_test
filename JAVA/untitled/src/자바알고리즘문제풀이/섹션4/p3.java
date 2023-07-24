de49687e67c5b11ff530f58fcf4361dbc84bd06f
fnzl54 <rnjscksdud1@naver.com>
2023-07-24 23:59:39 +0900

    feat : 4p3

---
+++ JAVA/untitled/src/자바알고리즘문제풀이/섹션4/p3.java
@@ -0,0 +1,50 @@
+package 자바알고리즘문제풀이.섹션4;
+
+import java.util.*;
+
+public class p3 {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int K = sc.nextInt();
+    int[] li = new int[N];
+    int left = 0, right = K;
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    List<Integer> answer = new ArrayList<Integer>();
+
+    for (int i = 0; i < N ; i++) {
+      li[i] = sc.nextInt();
+    }
+
+    for (int i = 0; i < K; i++) {
+      map.put(li[i], map.getOrDefault(li[i], 0) + 1);
+    }
+
+
+    while (right != N) {
+      int temp = 0;
+      for (int c : map.keySet()) {
+        if (map.get(c) != 0) {
+          temp++;
+        }
+      }
+      answer.add(temp);
+      map.put(li[left], map.get(li[left]) - 1);
+      map.put(li[right], map.getOrDefault(li[right], 0) + 1);
+      left++;
+      right++;
+    }
+
+    int temp = 0;
+    for (int c : map.keySet()) {
+      if (map.get(c) != 0) {
+        temp++;
+      }
+    }
+    answer.add(temp);
+
+    System.out.print(answer);
+
+  }
+}
