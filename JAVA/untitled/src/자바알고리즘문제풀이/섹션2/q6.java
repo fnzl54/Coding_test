989b298eb1c658123292300bd7d65b1a3c011e62
fnzl54 <rnjscksdud1@naver.com>
2023-08-10 23:34:36 +0900

    feat : 2p6

---
+++ JAVA/untitled/src/자바알고리즘문제풀이/섹션2/q6.java
@@ -0,0 +1,39 @@
+package 자바알고리즘문제풀이.섹션2;
+
+import java.util.*;
+class q6 {
+  public boolean isPrime(int num){
+    if(num==1) return false;
+    for(int i=2; i<num; i++){
+      if(num%i==0) return false;
+    }
+    return true;
+  }
+
+  public ArrayList<Integer> solution(int n, int[] arr){
+    ArrayList<Integer> answer = new ArrayList<>();
+    for(int i=0; i<n; i++){
+      int tmp=arr[i];
+      int res=0;
+      while(tmp>0){
+        int t=tmp%10;
+        res=res*10+t;
+        tmp=tmp/10;
+      }
+      if(isPrime(res)) answer.add(res);
+    }
+    return answer;
+  }
+  public static void main(String[] args){
+    q6 T = new q6();
+    Scanner kb = new Scanner(System.in);
+    int n=kb.nextInt();
+    int[] arr=new int[n];
+    for(int i=0; i<n; i++){
+      arr[i]=kb.nextInt();
+    }
+    for(int x : T.solution(n, arr)){
+      System.out.print(x+" ");
+    }
+  }
+}
