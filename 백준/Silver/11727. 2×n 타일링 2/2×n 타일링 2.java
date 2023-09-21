b7c79871a9ece39ced9705a470cefc652481e207
fnzl54 <85063965+fnzl54@users.noreply.github.com>
2023-09-22 02:45:43 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] case_list = new int[1001];
+
+    case_list[1] = 1;
+    case_list[2] = 3;
+
+    for (int i = 3; i < 1001; i++) {
+      case_list[i] = (case_list[i-1] + case_list[i-2] * 2) % 10007;
+    }
+
+    System.out.println(case_list[n]);
+  }
+}
