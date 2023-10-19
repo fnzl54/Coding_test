41088d482f31f02fd9aa82d565a1210f51e45ea5
fnzl54 <85063965+fnzl54@users.noreply.github.com>
2023-10-19 23:03:08 +0900

    [Silver I] Title: 스티커, Time: 700 ms, Memory: 110532 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int T = Integer.parseInt(br.readLine());
+
+    for (int t = 0; t < T; t++) {
+      int N = Integer.parseInt(br.readLine());
+      int[][] input = new int[2][N+1];
+      int[][] dp = new int[2][N+1];
+      for (int i = 0; i < 2; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int j = 1; j <= N; j++) {
+          input[i][j] = Integer.parseInt(st.nextToken());
+        }
+      }
+
+      dp[0][1] = input[0][1];
+      dp[1][1] = input[1][1];
+
+      for (int i = 2; i <= N; i++) {
+        dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2]) + input[0][i];
+        dp[1][i] = Math.max(dp[0][i-1], dp[0][i-2]) + input[1][i];
+      }
+
+      sb.append(Math.max(dp[0][N], dp[1][N])).append("\n");
+    }
+
+    System.out.println(sb);
+  }
+}
