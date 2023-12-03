d20d988051ec6539df9e9f87ee4d9169a069d56e
fnzl54 <85063965+fnzl54@users.noreply.github.com>
2023-12-04 00:29:27 +0900

    [Silver II] Title: N과 M (11), Time: 1256 ms, Memory: 177536 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+  static int N, M;
+  static int[] arr;
+  static List<Integer> input = new ArrayList<>();
+  static Set<String> answer = new HashSet<>();
+  static List<String> answer_list = new ArrayList<>();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    st = new StringTokenizer(br.readLine());
+    for (int n = 0; n < N; n++) {
+      input.add(Integer.parseInt(st.nextToken()));
+    }
+
+    Collections.sort(input);
+    arr = new int[M];
+
+    dfs(0);
+
+
+    for (String s : answer_list) {
+      sb.append(s).append("\n");
+    }
+
+    System.out.println(sb);
+  }
+
+  static void dfs (int depth) {
+    if (depth == M) {
+      StringBuilder temp_sb = new StringBuilder();
+      for (int i : arr) {
+        temp_sb.append(i).append(" ");
+      }
+      if (!answer.contains(temp_sb.toString())) {
+        answer.add(temp_sb.toString());
+        answer_list.add(temp_sb.toString());
+      }
+
+      return;
+    }
+
+    for (int n = 0; n < input.size(); n++) {
+      arr[depth] = input.get(n);
+      dfs(depth + 1);
+    }
+  }
+
+}
