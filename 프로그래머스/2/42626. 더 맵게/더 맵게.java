deccf9085a0fdc642142295b7b01ed8d282e543c
fnzl54 <85063965+fnzl54@users.noreply.github.com>
2024-02-21 21:26:54 +0900

    [level 2] Title: 더 맵게, Time: 1667.88 ms, Memory: 147 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        
+        for (int i : scoville) {
+            q.add(i);
+        }
+        
+        while(q.peek() < K) {
+            if (q.size() == 1) {
+                return -1;
+            }
+            
+            q.add(q.poll() + q.poll() * 2);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
