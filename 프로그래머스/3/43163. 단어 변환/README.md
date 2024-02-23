aa6a421e23f2030af97572ab0879f3d197fa1eb1
fnzl54 <85063965+fnzl54@users.noreply.github.com>
2024-02-24 01:28:46 +0900

    [level 3] Title: 단어 변환, Time: 0.18 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 74.7 MB, 시간: 0.03 ms
+메모리: 73.7 MB, 시간: 0.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 0일 1:4:18
+2024년 02월 24일 01:28:44
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,42 +1,38 @@
 import java.util.*;
 
 class Solution {
-    private String endWord;
-    private int answer = Integer.MAX_VALUE;
-    private boolean[] check;
-    
+    static int answer = 0;
     
     public int solution(String begin, String target, String[] words) {
-        endWord = target;
-        check = new boolean[words.length];
         
-        dfs(0, begin, words);
+        bfs(begin, target, words);
         
-        if (answer == Integer.MAX_VALUE) {
-            return 0;
-        } else {
-            return answer;   
-        }
+        return answer;
     }
     
-    private void dfs (int depth, String nowWord, String[] words) {
-        if (depth == words.length+1) {
-            return;
-        }
+    private void bfs(String begin, String target, String[] words) {
+        Queue<Object[]> q = new LinkedList<>();
+        boolean[] check = new boolean[words.length];
+        q.add(new Object[] {begin, 0});
         
-        if (nowWord.equals(endWord)) {
-            answer = Math.min(depth, answer);
-            return;
-        } else {
+        while(!q.isEmpty()) {
+            Object[] now = q.poll();
+            String nowWord = (String)now[0];
+            int count = (int)now[1];
+            if (nowWord.equals(target)) {
+                answer = count;
+                q.clear();
+                break;
+            }
+            
             for (int i = 0; i < words.length; i++) {
                 if (!check[i] && checkWordOneDiff(nowWord, words[i])) {
+                
                     check[i] = true;
-                    dfs (depth+1, words[i], words);
-                    check[i] = false;
+                    q.add(new Object[] {words[i], count+1});
                 }
             }
         }
-        
         
     }
     
@@ -55,6 +51,5 @@ class Solution {
             return false;
         }
     }
-        
-        
 }
+
